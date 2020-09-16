c7bf98fbe1449012661a202ad95ee56416e1cccd
jeffrey <jeffreycao1998@hotmail.com>
2020-09-16 14:50:23 -0400

    created a dashboard for admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate, :only => [ :show ]
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
   private
